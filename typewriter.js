c7cd9ff356aba09e5aeb2a4a87bba17b8b280b19
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-19 15:44:11 -0500

    Added new line

--- typewriter.js
+++ typewriter.js
@@ -13,6 +13,24 @@ const speed = 50;
 for (let i = 0; i < sentence.length; i++) {
   setTimeout( () =>{
     process.stdout.write(sentence[i]);
+    if (i === sentence.length-1){
+      process.stdout.write('\n'); // \n stands for jumping to the next command line
+    }
    // console.log(sentence[i]);
   }, i*speed);
+ 
 }
+
+
+// const sentence = "hello there from lighthouse labs";
+// const delay = 50;
+
+// for (let i = 0; i < sentence.length; i++) {
+//   setTimeout(() => {
+//     process.stdout.write(sentence[i]);
+//     if (i === sentence.length - 1) {
+//       // add a newline character after printing the last letter
+//       process.stdout.write("\n");
+//     }
+//   }, i * delay);
+// }
